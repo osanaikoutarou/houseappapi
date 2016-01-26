392cbaf849a989bad15e2ba1342ad2dca3286c27
osanaikoutarou <netetahito@gmail.com>
2016-01-26 16:01:16 +0900

    eXgiK@S3ɉ摜Abvµ½̂ŁAAPIÌseedðύX

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,11 @@
 #Photo.create(:liked_count=>456 , :passed_count => 5432);
 #Photo.create(:image_url=>'http://imageurl003' , :title=>'タイトル03' , :like=>true , :pass=>false , :liked_count=>789 , :passed_count => 1234 , :description=>"説明文テスト03");
 
-Photo.create(:id=>'012', :uuid=>'212121', :title=>'たいとる', :image_url=>'がぞうurl', :liked_count=>123, :passed_count=>234, :description=>'説明文');
-Photo.create(:id=>'013', :uuid=>'abcdef', :title=>'タイトル', :image_url=>'がぞうurl', :liked_count=>123, :passed_count=>234, :description=>'説明文');
+Photo.create(:id=>'012', :uuid=>'abcde', :title=>'たいとる111', :image_url=>'a000001_h0000001_img000000001.jpg', :liked_count=>12, :passed_count=>2345, :description=>'説明文1説明文');
+Photo.create(:id=>'013', :uuid=>'fghijk', :title=>'タイトル222', :image_url=>'a000001_h0000001_img000000002.jpg', :liked_count=>123, :passed_count=>23456, :description=>'説明文2説明文');
+Photo.create(:id=>'013', :uuid=>'lmnopq', :title=>'たいとる333', :image_url=>'a000001_h0000001_img000000001.jpg', :liked_count=>1234, :passed_count=>234567, :description=>'説明文3説明文');
+Photo.create(:id=>'014', :uuid=>'rstuvw', :title=>'タイトル444', :image_url=>'a000001_h0000001_img000000002.jpg', :liked_count=>12345, :passed_count=>2345678, :description=>'説明文4説明文');
+Photo.create(:id=>'015', :uuid=>'xyz012', :title=>'タイトル555', :image_url=>'a000001_h0000001_img000000002.jpg', :liked_count=>123456, :passed_count=>23456789, :description=>'説明文5説明文');
 
 #Book.create(:title=>"たいとる01");
 
