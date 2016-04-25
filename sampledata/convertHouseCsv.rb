a923195b9b3680b1c86363e60eb10e7842c9be8e
長内 幸太郎 <k_osanai@waku-2.com>
2016-04-25 19:07:55 +0900

    seedðcsv»@Ƃ肠¦¸t@C¾¯

---
+++ sampledata/convertHouseCsv.rb
@@ -0,0 +1,67 @@
+require 'csv'
+data_list = Array.new
+
+c = 1
+bufDescription = ""
+
+CSV.foreach("houses.csv") do |data|
+
+  # header
+  if c==1
+    c+=1
+    next
+  end
+
+  if data[0].to_s.length>0
+    isHeader = true       # IDとかあるヘッダー行
+    isAddInfo = false     # それ以外のはみ出した行
+  else
+    isHeader = false
+    isAddInfo = true
+  end
+
+  if isHeader
+    architectID   = data[0]
+    architectName = data[1]
+    houseNo       = data[2]
+    houseID       = data[3]
+    name          = data[4]
+    cost          = data[5]
+    area          = data[6]
+    space         = data[7]
+    description   = data[8]
+    suvacoURL     = data[9]
+
+  end
+  # どちみち
+  description   = data[8]
+
+  if isHeader
+    ex_data = [architectID,architectName,houseNo,name,cost,area,space,bufDescription,suvacoURL]
+    data_list.push(ex_data)
+
+    bufDescription = ""
+  end
+
+  if isAddInfo
+    bufDescription += (description.to_s + "\r\n")
+  end
+  c+=1
+end
+
+file_name = "convertedHouseCSV.csv"    #保存するファイル
+
+File.open(file_name, 'w') {|file|
+  data_list.each do |data|
+
+    writeStr=""
+    data.each do |element|
+      writeStr += ('"' + element.to_s + '"' + ",")
+    end
+
+    file.write writeStr.chop+"\r\n"
+
+  end
+}
+
+puts("end")
---
+++ sampledata/convertcsv.rb
@@ -0,0 +1,79 @@
+require 'csv'
+data_list = Array.new
+
+c = 1
+bufMessage = ""   # メッセージマージ
+bufCareer = ""    # 経歴マージ
+bufPolicy = ""    # こだわりマージ
+
+CSV.foreach("architects.csv") do |data|
+
+  # header
+  if c==1||c==2
+    c+=1
+    next
+  end
+
+  if data[0].to_s.length>0
+    isHeader = true       # IDとかあるヘッダー行
+    isAddInfo = false     # それ以外のはみ出した行
+  else
+    isHeader = false
+    isAddInfo = true
+  end
+
+  if isHeader
+    architectID   = data[0]
+    name          = data[1]
+    affiliation   = data[2]
+    addPost       = data[3]
+    addPref       = data[4]
+    addCity       = data[5]
+    addOther      = data[6]
+    qualifications= data[7]
+    message       = data[8]
+    policy        = data[9]
+    goodAtType    = data[10]
+    career        = data[11]
+    suvacoURL     = data[12]
+    architectURL  = data[13]
+
+  end
+  # どちみち
+  message       = data[8]
+  policy        = data[9]
+  career        = data[11]
+
+  if isHeader
+    ex_data = [architectID,name,affiliation,addPost,addPref,addCity,addOther,qualifications,
+    bufMessage,policy,goodAtType,bufCareer,suvacoURL,architectURL]
+    data_list.push(ex_data)
+
+    bufMessage = ""
+    bufCareer = ""
+  end
+
+  if isAddInfo
+    bufMessage += (message.to_s + "\r\n")
+    bufCareer += (career.to_s + "\r\n")
+    bufPolicy += (policy.to_s + "\r\n")
+  end
+  c+=1
+end
+
+file_name = "convertedArchitectCSV.csv"    #保存するファイル
+
+File.open(file_name, 'w') {|file|
+  data_list.each do |data|
+
+    writeStr=""
+    data.each do |element|
+      writeStr += ('"' + element.to_s + '"' + ",")
+    end
+
+    file.write writeStr.chop+"\r\n"
+
+  end
+}
+
+puts("end")
